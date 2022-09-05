d74ecda0341f45a7b639fc0305f9063573ff9514
DavidAnacona22 <david.a@landian.io>
2022-09-04 23:17:27 -0500

    seccion two

--- src/App.js
+++ src/App.js
@@ -32,31 +32,26 @@ function App() {
             <Typography sx={{ fontSize: '30px', fontWeight: '200', lineHeight: '40px', color: 'white', marginTop: '40px' }}>TAKE ADVANTAGE OF <b>THE METAVERSE WITH YOUR LANDS.</b></Typography>
           </Stack>
         </Stack>
-        <Stack sx={{ width: '100%', height: '100vh' }}>
-          <Stack sx={{ width: '100%', flexDirection: 'row', alignItems: 'center', height: '120px', justifyContent: 'space-between' }}>
-            <Box sx={{ marginTop: '180px' }}>
-              <img src='./assets/landian-text-logo.svg' alt='LANDIAN' />
-            </Box>
-            <Box sx={{ marginRight: '100px' }}>
-              <Button sx={{ backgroundColor: 'white', color: 'black', fontSize: '10px', fontWeight: '400', width: '153px', height: '40px', borderRadius: '60px', marginRight: '40px' }}>WHITEPAPER</Button>
-              <Button sx={{ backgroundColor: 'black', color: 'white', fontSize: '10px', fontWeight: '400', width: '153px', height: '40px', borderRadius: '60px', border: '1px solid white' }}>HOW TO BUY IT</Button>
-            </Box>
-          </Stack>
+        <Stack sx={{ width: '100%', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
           <Stack sx={{ color: 'white', fontSize: '32px', fontWeight: '400', marginTop: '50px' }}>
-            <Typography variant='h4'>LANDIAN <b>STARTS HERE, WITH YOU.</b></Typography>
+            <Typography variant='h4'>OUR <b>MARKETPLACE</b> GUIDES YOU <b>TO THE GATES OF LANDIAN</b></Typography>
           </Stack>
-          <Stack sx={{ flexDirection: 'row', alignItems: 'center', marginTop: '-190px' }}>
-            <img src='./assets/hand 1.png' alt='LANDIAN' style={{ width: '1000px', height: '1100px', marginLeft: '-400px' }} />
-            <Box sx={{ width: '950px', height: '345px', marginLeft: '-130px', marginTop: '-250px' }}>
-              <ReactPlayer url='https://youtu.be/s0rHe0uw-V8' width='100%' height='100%' controls />
+          <Stack sx={{ flexDirection: 'row', color: 'white', marginTop: '80px' }}>
+            <Box sx={{ border: '1px solid white', borderRadius: '103px', width: '333px', height: '449px', marginRight: '50px' }}>
+              <img src='./assets/seccion2-money.png' alt='LANDIAN' style={{ width: '500px', height: '409px', marginLeft: '-80px' }} />
+              <Typography sx={{ marginTop: '-40px', fontSize: '20px', opacity: '0.8' }}>1. BUY <b>$LNDA</b> NOW</Typography>
+            </Box>
+            <Box sx={{ border: '1px solid white', borderRadius: '103px', width: '333px', height: '449px', marginRight: '50px' }}>
+              <img src='./assets/seccion2-golden.png' alt='LANDIAN' style={{ width: '300px', height: '400px', transform: 'rotate(-10.6deg)', marginTop: '10px' }} />
+              <Typography sx={{ marginTop: '-40px', fontSize: '12px', opacity: '0.8' }}>2. DECIDE <b>WHAT YOU'RE GOING</b></Typography>
+              <Typography sx={{ fontSize: '12px', opacity: '0.5' }}><b>TO BUILD IN LANDIAN</b></Typography>
+            </Box>
+            <Box sx={{ border: '1px solid white', borderRadius: '103px', width: '333px', height: '449px' }}>
+              <img src='./assets/seccion2-planet.png' alt='LANDIAN' style={{ width: '367px', height: '307px', marginLeft: '-16px', marginTop: '55px' }} />
+              <Typography sx={{ marginTop: '-40px', fontSize: '12px', opacity: '0.8', marginTop: '5px' }}>3. SET UP THE <b>LANDIAN ECOSYSTEM</b></Typography>
             </Box>
           </Stack>
-          <Stack sx={{ marginTop: '-450px', alignItems: 'center' }}>
-            <Typography sx={{ color: 'white', fontSize: '20px', fontWeight: '300' }}>You are a few steps away from <b>buying land in Landian.</b></Typography>
-            <Typography sx={{ color: 'white', fontSize: '20px', fontWeight: '300' }}>You can now <b>dream about</b> what you will build in this metaverse.</Typography>
-            <Button sx={{ backgroundColor: 'black', color: 'white', fontSize: '20px', fontWeight: '700', width: '233px', height: '75px', borderRadius: '100px', border: '1px solid white', marginTop: '40px' }}>BUY LNDA</Button>
-            <Typography sx={{ fontSize: '30px', fontWeight: '200', lineHeight: '40px', color: 'white', marginTop: '40px' }}>TAKE ADVANTAGE OF <b>THE METAVERSE WITH YOUR LANDS.</b></Typography>
-          </Stack>
+          <Button sx={{ backgroundColor: 'black', color: 'white', fontSize: '20px', fontWeight: '700', width: '233px', height: '75px', borderRadius: '100px', border: '1px solid white', marginTop: '100px' }}>LET'S START!</Button>
         </Stack>
       </Stack>
     </div >
